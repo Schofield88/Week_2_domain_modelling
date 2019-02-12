086ca0501cfb885b1001e5979efab4fad79b8e08
Luke Phyall <lukeprospect@yahoo.com>
2019-02-12 10:53:58 +0000

    Adds Coin and Score class, print method for Score, and that's about it.

---
+++ lib/coin.rb
@@ -0,0 +1,23 @@
+class Coin
+
+  def flip
+    toss = rand(1..2)
+    toss == 1 ? "Heads" : "Tails"
+
+  end
+
+end
+
+class Score
+
+  attr_accessor :score
+
+  def initialize
+    @score = []
+  end
+
+  def print
+    puts @score
+  end
+  
+end
